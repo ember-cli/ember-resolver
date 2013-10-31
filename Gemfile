8757fb74febb665be9fa21eb58027295a7b2d076
Robert Jackson <robert.w.jackson@me.com>
2013-10-30 21:00:07 -0400

    Begin fleshing out directory structure.

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'rake-pipeline', :git => 'https://github.com/livingsocial/rake-pipeline.git'
+gem 'ember-dev', :git => 'https://github.com/emberjs/ember-dev.git', :branch => 'master'
+
+gem 'ember-source', '~> 1.2.0.beta.2'
---
+++ generators/license.js
@@ -0,0 +1,6 @@
+// ==========================================================================
+// Project:   Ember - JavaScript Application Framework
+// Copyright: Copyright 2013 Stefan Penner and Ember App Kit Contributors
+// License:   Licensed under MIT license
+//            See https://raw.github.com/stefanpenner/ember-jj-abrams-resolver/master/LICENSE
+// ==========================================================================
