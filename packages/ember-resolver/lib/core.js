0ae6c8cad17166ecf98cb71b262100e842a32e69
Joshua Kelly <jdjkelly@gmail.com>
2014-08-01 12:19:36 -0400

    Resolves IE8 bug

--- packages/ember-resolver/lib/core.js
+++ packages/ember-resolver/lib/core.js
@@ -33,7 +33,7 @@ define("ember/resolver",
     };
   }
 
-  if (!Object.create && !Object.create(null).hasOwnProperty) {
+  if (!(Object.create && !Object.create(null).hasOwnProperty)) {
     throw new Error("This browser does not support Object.create(null), please polyfil with es5-sham: http://git.io/yBU2rg");
   }
 
