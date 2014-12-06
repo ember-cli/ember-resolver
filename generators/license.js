f452cec0ea2b2da8888a0848cbaefd021f4d5a63
Ryunosuke SATO <tricknotes.rs@gmail.com>
2014-12-06 22:00:46 +0900

    Fix link to homepage
    Currently, `ember-jj-abrams-resolver` becomes `ember-resolver`.

--- generators/license.js
+++ generators/license.js
@@ -2,5 +2,5 @@
 // Project:   Ember - JavaScript Application Framework
 // Copyright: Copyright 2013 Stefan Penner and Ember App Kit Contributors
 // License:   Licensed under MIT license
-//            See https://raw.github.com/stefanpenner/ember-jj-abrams-resolver/master/LICENSE
+//            See https://raw.github.com/stefanpenner/ember-resolver/master/LICENSE
 // ==========================================================================
