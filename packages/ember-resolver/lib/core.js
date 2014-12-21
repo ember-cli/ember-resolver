ea15c4b8955476fb120d6054308d417b0eb4a3d6
Robert Jackson <robert.w.jackson@me.com>
2014-12-20 21:24:49 -0500

    Also, stamp instances with moduleBasedResolver.

--- packages/ember-resolver/lib/core.js
+++ packages/ember-resolver/lib/core.js
@@ -153,6 +153,7 @@ define("ember/resolver",
     },
     init: function() {
       this._super();
+      this.moduleBasedResolver = true;
       this._normalizeCache = makeDictionary();
 
       this.pluralizedTypes = this.pluralizedTypes || makeDictionary();
