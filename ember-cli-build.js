51d66edaedde48493b57da5521a30eb51d3fa1bc
Chris Thoburn <runspired@users.noreply.github.com>
2015-12-17 15:33:17 -0600

    fix(bower.json): removes circular `ember-resolver` dependency.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,8 +1,10 @@
+/*jshint node:true*/
 /* global require, module */
 var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
+    // Add options here
   });
 
   /*
