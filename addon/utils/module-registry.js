73df9720776529970cddf1c65f2f4163c64cc5a7
Krati Ahuja <krahuja@linkedin.com>
2016-06-02 11:25:07 -0700

    Make resolver understand string modules
    1. Removes unecessary initializer which is used in ember extensions
    2. Removes the need to combine the registries since resolver now understands string module.

--- addon/utils/module-registry.js
+++ addon/utils/module-registry.js
@@ -7,15 +7,20 @@ if (typeof requirejs.entries === 'undefined') {
 }
 
 function ModuleRegistry(entries) {
-  this._entries = entries || requirejs.entries;
+  this._entries = requirejs.entries;
+  this._stringRegistry = requirejs.stringRegistry;
 }
 
 ModuleRegistry.prototype.moduleNames = function ModuleRegistry_moduleNames() {
-  return (Object.keys || Ember.keys)(this._entries);
+  let moduleNames = [];
+  let entries = (Object.keys || Ember.keys)(this._entries);
+  let stringRegistry = (Object.keys || Ember.keys)(this._stringRegistry);
+  moduleNames.push.apply(entries, stringRegistry);
+  return moduleNames;
 };
 
 ModuleRegistry.prototype.has = function ModuleRegistry_has(moduleName) {
-  return moduleName in this._entries;
+  return this._entries[moduleName] || this._stringRegistry[moduleName];
 };
 
 ModuleRegistry.prototype.get = function ModuleRegistry_get(moduleName, exportName = 'default') {
