e6c6eb52da9522bbc51220ef91d5f9f15e8eacbe
Cory <cory.forsyth@gmail.com>
2016-10-07 18:12:43 -0400

    Add treeForApp hook to mv src/ to app/src/

--- index.js
+++ index.js
@@ -2,6 +2,14 @@
 'use strict';
 
 var VersionChecker = require('ember-cli-version-checker');
+var Funnel = require('broccoli-funnel');
+var MergeTrees = require('broccoli-merge-trees')
+
+var renameMap = {
+  'src/main.js': 'app.js',
+  'src/resolver.js': 'resolver.js',
+  'src/ui/styles/app.css': 'styles/app.css' // Need a glob strategy
+};
 
 module.exports = {
   name: 'dangerously-set-unified-resolver',
@@ -21,6 +29,24 @@ module.exports = {
     }
 
     this.app.import('vendor/ember-resolver/legacy-shims.js');
+  },
+
+  treeForApp() {
+    var srcTree = new Funnel('src', {
+      destDir: 'src'
+    });
+
+    var appTree = new Funnel('app');
+
+    var unifiedTree = new MergeTrees([appTree, srcTree]);
+
+    var withAppCompatibility = new Funnel(unifiedTree, {
+      getDestinationPath: function getDestinationPath(relativePath) {
+        return renameMap[relativePath] || relativePath;
+      }
+    });
+
+    return withAppCompatibility;
   },
 
   monkeyPatchVendorFiles: function() {
