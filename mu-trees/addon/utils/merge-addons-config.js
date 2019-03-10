00ba6fca7d17c5847bad5d2fe463aa55dee5ac09
Pepe Cano <825430+ppcano@users.noreply.github.com>
2019-03-10 06:35:36 +0100

    Fix `mergeAddonsConfig` comments

--- mu-trees/addon/utils/merge-addons-config.js
+++ mu-trees/addon/utils/merge-addons-config.js
@@ -1,11 +1,11 @@
-/**
-   This function provides the types and collections from addons `addonsConfig` to `ember-config`.
+/*
+   This function merges the types and collections from addons `addonsConfig` into `config`.
 
    It will throw an exception if an addon tries to override
    an existing type or collection on the resolver config object or
    if two addons provide the same type or collection.
 
-   - `config`: is the resolver config generated with `ember-resolver/ember-config`
+   - `config`: is a resolver config object.
 
    - `addonsConfig`: is a hash object containing the result of the call of
       `addon.resolverConfig` method on all the project addons.
@@ -17,13 +17,9 @@
       hola-addon: { types: { ... }, collections: { ... } }
      }
      ```
-
-     The value of `addonsConfig` is be imported from 'ember-resolver/addons-config'.
  */
 export default function mergeAddonsConfig(config, addonsConfig) {
 
-  // This implementation does not allow an addon to overwrite the default MU module resolution.
-  // Is this the expected behaviour?
   Object.keys(addonsConfig).forEach(function (addonName) {
     let addonConfig = addonsConfig[addonName];
     let addonTypes = addonConfig.types || {};
@@ -33,7 +29,6 @@ export default function mergeAddonsConfig(config, addonsConfig) {
       if (!config.types.hasOwnProperty(key)) {
         config.types[key] = addonTypes[key];
       } else {
-        // A similar validation is done during the build phase on `index.validateAddonsConfig`
         throw new Error(`Addon '${addonName}' attempts to configure the type '${key}' on the resolver but '${key}' has already been configured.`);
       }
     });
@@ -41,7 +36,6 @@ export default function mergeAddonsConfig(config, addonsConfig) {
       if (!config.collections.hasOwnProperty(key)) {
         config.collections[key] = addonCollections[key];
       } else {
-        // A similar validation is done during the build phase on `index.validateAddonsConfig`
         throw new Error(`Addon '${addonName}' attempts to configure the collection '${key}' on the resolver but '${key}' has already been configured.`);
       }
     });
