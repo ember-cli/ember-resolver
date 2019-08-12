cb5aadb1ebc3575e624f36147c2a5ef7b9c90ea8
Godfrey Chan <godfreykfc@gmail.com>
2019-08-12 14:46:34 -0700

    Use `require.has` to support `*/index` fallback
    This is required for https://github.com/emberjs/ember.js/issues/18255

--- addon/resolvers/classic/index.js
+++ addon/resolvers/classic/index.js
@@ -20,7 +20,7 @@ export class ModuleRegistry {
     return Object.keys(this._entries);
   }
   has(moduleName) {
-    return moduleName in this._entries;
+    return require.has(moduleName);
   }
   get(moduleName) {
     return require(moduleName);
