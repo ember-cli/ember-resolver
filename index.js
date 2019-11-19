dc0e79cbf3f7bbd0a7ef52598f55c9695d200585
Stefan Penner <stefan.penner@gmail.com>
2019-11-19 14:24:04 -0700

    Remove dead module unification code

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 'use strict';
 
 const VersionChecker = require('ember-cli-version-checker');
-const path = require('path');
-let isModuleUnification;
 
 module.exports = {
   name: require('./package.json').name,
@@ -24,7 +22,6 @@ module.exports = {
       }
     }
     this._emberResolverFeatureFlags = this.emberResolverFeatureFlags();
-    isModuleUnification = !!this.project.isModuleUnification && this.project.isModuleUnification();
 
     this.options.babel = {
       loose: true,
@@ -45,45 +42,6 @@ module.exports = {
         }]
       ]
     };
-  },
-
-  treeForAddon() {
-    const MergeTrees = require('broccoli-merge-trees');
-    const addonTrees = [].concat(
-      this._super.treeForAddon.apply(this, arguments),
-      isModuleUnification && this._moduleUnificationTrees()
-    ).filter(Boolean);
-
-    return new MergeTrees(addonTrees);
-  },
-
-  _moduleUnificationTrees() {
-    const resolve = require('resolve');
-    const Funnel = require('broccoli-funnel');
-
-    let featureTreePath = path.join(this.root, 'mu-trees/addon');
-    let featureTree = new Funnel(featureTreePath, {
-      destDir: 'ember-resolver'
-    });
-
-    const glimmerResolverSrc = require.resolve('@glimmer/resolver/package');
-    let glimmerResolverPath = path.dirname(glimmerResolverSrc);
-    let glimmerResolverTree = new Funnel(glimmerResolverPath, {
-      srcDir: 'dist/modules/es2017',
-      destDir: '@glimmer/resolver'
-    });
-
-    let glimmerDISrc = resolve.sync('@glimmer/di', { basedir: glimmerResolverPath });
-    let glimmerDITree = new Funnel(path.join(glimmerDISrc, '../../../..'), {
-      srcDir: 'dist/modules/es2017',
-      destDir: '@glimmer/di'
-    });
-
-    return [
-      this.preprocessJs(featureTree, { registry: this.registry }),
-      this.preprocessJs(glimmerResolverTree, { registry: this.registry }),
-      this.preprocessJs(glimmerDITree, { registry: this.registry }),
-    ];
   },
 
   included() {
