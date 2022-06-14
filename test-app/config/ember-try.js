88b12c9bb2f0f45c6f4c6e16de61573e037d5def
Robert Jackson <rjackson@linkedin.com>
2022-06-14 11:47:31 -0400

    Fix incorrect `config/` folder processing from earlier step

---
+++ test-app/config/ember-try.js
@@ -0,0 +1,105 @@
+'use strict';
+
+const getChannelURL = require('ember-source-channel-url');
+
+const EMBROIDER_VERSION = '^1.8.0';
+const embroider = {
+  safe: {
+    name: 'embroider-safe',
+    npm: {
+      devDependencies: {
+        '@embroider/core': EMBROIDER_VERSION,
+        '@embroider/webpack': EMBROIDER_VERSION,
+        '@embroider/compat': EMBROIDER_VERSION,
+        '@embroider/test-setup': EMBROIDER_VERSION,
+      },
+    },
+    env: {
+      EMBROIDER_TEST_SETUP_OPTIONS: 'safe',
+    },
+  },
+
+  optimized: {
+    name: 'embroider-optimized',
+    npm: {
+      devDependencies: {
+        '@embroider/core': EMBROIDER_VERSION,
+        '@embroider/webpack': EMBROIDER_VERSION,
+        '@embroider/compat': EMBROIDER_VERSION,
+        '@embroider/test-setup': EMBROIDER_VERSION,
+      },
+    },
+    env: {
+      EMBROIDER_TEST_SETUP_OPTIONS: 'optimized',
+    },
+  },
+};
+
+
+module.exports = async function() {
+  return {
+    useYarn: true,
+    scenarios: [
+      {
+        name: 'ember-lts-3.24',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.24.0'
+          }
+        }
+      },
+      {
+        name: 'ember-lts-3.28',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.28.0'
+          }
+        }
+      },
+      {
+        name: 'ember-lts-4.4',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.4.0'
+          }
+        }
+      },
+      {
+        name: 'ember-release',
+        npm: {
+          devDependencies: {
+            'ember-source': await getChannelURL('release')
+          }
+        }
+      },
+      {
+        name: 'ember-beta',
+        npm: {
+          devDependencies: {
+            'ember-source': await getChannelURL('beta')
+          }
+        }
+      },
+      {
+        name: 'ember-canary',
+        npm: {
+          devDependencies: {
+            'ember-source': await getChannelURL('canary')
+          }
+        }
+      },
+      // The default `.travis.yml` runs this scenario via `yarn test`,
+      // not via `ember try`. It's still included here so that running
+      // `ember try:each` manually or from a customized CI config will run it
+      // along with all the other scenarios.
+      {
+        name: 'ember-default',
+        npm: {
+          devDependencies: {}
+        }
+      },
+      embroider.safe,
+      embroider.optimized,
+    ]
+  };
+};
