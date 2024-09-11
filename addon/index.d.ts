a22a537d97d9b427fcbbd857901ecb0aac9f05a4
Edward Faulkner <edward@eaf4.com>
2024-09-11 18:37:35 +0100

    updating d.ts

--- addon/index.d.ts
+++ addon/index.d.ts
@@ -1,6 +1,8 @@
 import { Resolver as ResolverContract } from "@ember/owner";
-import EmberObject from "@ember/object";
-export default class Resolver extends EmberObject {}
+
+export default class Resolver {
+  static create(props: Record<string, unknown>): InstanceType<this>;
+}
 export default interface Resolver extends Required<ResolverContract> {
     pluralizedTypes: Record<string, string>;
 }
